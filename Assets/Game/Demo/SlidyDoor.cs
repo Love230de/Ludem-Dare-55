d4d4e5a664cce9cc34910e70697ad5024ac13d04
Edward Curtis <gamerguy1835@yahoo.com>
2024-04-13 18:00:57 -0400

    Added the potential for a sliding door

---
+++ Assets/Game/Demo/SlidyDoor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidyDoor : MonoBehaviour
+{
+    //Trying to figure out how to approach a sliding door
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
