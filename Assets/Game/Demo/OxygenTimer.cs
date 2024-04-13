8d764aefa1edd83724fc17b95a2c8551df396d78
Edward Curtis <gamerguy1835@yahoo.com>
2024-04-13 19:15:46 -0400

    Added Gauge Functionality

--- Assets/Game/Demo/OxygenTimer.cs
+++ Assets/Game/Demo/OxygenTimer.cs
@@ -6,22 +6,37 @@ public class OxygenTimer
 {
     private bool empty { get; set; }
     private float seconds;
+    public GameObject GaugeArrow;
     public bool Empty { get { return empty; } }
     public float OxygenAmount { get { return seconds; } }   
     public float maxSeconds;
    
+
+    void GaugeFunctionality()
+    {
+        float currentOxygen = (seconds / maxSeconds) * 100;
+
+        float angle = GaugeArrow.transform.localEulerAngles.z;
+
+        angle -= Time.deltaTime;
+
+        angle = Mathf.Clamp(angle, -currentOxygen * 0.90f, currentOxygen * 0.90f);
+        
+        GaugeArrow.transform.localEulerAngles = new Vector3(0, 0, angle); 
+    }
    public void UpdateOxygenTimer()
     {
     
         if(seconds > 0)
         {
             seconds -= Time.deltaTime;
-      
+            GaugeFunctionality();
         }
         else if(!empty) 
         {
             empty = true;
             seconds = maxSeconds;
         }
+       
     }
 }
