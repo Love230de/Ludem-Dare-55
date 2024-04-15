d22f477b6dc47051dad9b6cd77fadba54cfb4a12
Edward Curtis <gamerguy1835@yahoo.com>
2024-04-15 19:04:33 -0400

    Added Win Scene

---
+++ Assets/Game/Demo/Win.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Win : MonoBehaviour
+{
+    float resetTime = 1.0f;
+    float seconds = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * 200 * Time.deltaTime);
+
+      if(seconds < resetTime)
+        {
+            seconds += Time.deltaTime;
+        }
+      else
+        {
+          //  SceneManager.LoadScene(0);
+        }
+    }
+}
